ac2b99f0f4999ee00106c0d29289c53d03eb26a4
KY YAP <yapky-wm22@student.tarc.edu.my>
2023-09-26 11:23:03 +0800

    Updated Sound Settings

--- Game Assignment/Assets/Scipts/AudioManager.cs
+++ Game Assignment/Assets/Scipts/AudioManager.cs
@@ -8,38 +8,8 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    public UIManager ui;
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
-    public AudioMixer mixer;
-    private void Start()
-    {
-        PlayMusic("Theme");
-
-        // Load music and SFX volume settings from PlayerPrefs
-        float musicVolume = PlayerPrefs.GetFloat("VolumeMusic", 1.0f); // Default to 1.0 if not found
-        float sfxVolume = PlayerPrefs.GetFloat("VolumeSFX", 1.0f); // Default to 1.0 if not found
-
-        // Apply the loaded volume settings
-        MusicVolume(musicVolume);
-        SFXVolume(sfxVolume);
-
-        //// Check if 'ui' is assigned before accessing its fields
-        if (ui != null)
-        {
-            // Update the UI sliders with the loaded volume values
-            ui._musicSlider.value = musicVolume * 100.0f; // Convert to percentage
-            ui._sfxSlider.value = sfxVolume * 100.0f; // Convert to percentage
-        }
-    }
-
-    private void Update()
-    {
-        mixer.SetFloat("VolumeMusic", musicSource.volume);
-        PlayerPrefs.SetFloat("VolumeMusic", musicSource.volume);
-        mixer.SetFloat("VolumeSFX", sfxSource.volume);
-        PlayerPrefs.SetFloat("VolumeSFX", sfxSource.volume);
-    }
 
     private void Awake()
     {
@@ -52,6 +22,10 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    private void Start()
+    {
+        PlayMusic("Theme");
     }
 
     public void PlayMusic(string name)
@@ -68,9 +42,9 @@ public class AudioManager : MonoBehaviour
             musicSource.Play();
         }
     }
-    public void PlaySFX()
+    public void PlaySFX(string name)
     {
-        Sound s = Array.Find(musicSounds, x => x.name == name);
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
 
         if (s == null)
         {
@@ -89,31 +63,25 @@ public class AudioManager : MonoBehaviour
     {
         sfxSource.mute = !sfxSource.mute;
     }
-    public void MusicVolume(float volume)
+    public float MusicVolume(float volume)
     {
         // Ensure the input value is within the 0-100 range
         float volumePercentage = Mathf.Clamp(volume, 0.0f, 100.0f);
 
         // Convert the volumePercentage to the 0-1 range
-        volume = volumePercentage / 100.0f;
+        volume = volumePercentage/100;
         musicSource.volume = volume;
-
-        // Save the music volume to PlayerPrefs
-        PlayerPrefs.SetFloat("VolumeMusic", volume);
-        PlayerPrefs.Save(); // Explicitly save PlayerPrefs data
+        return musicSource.volume;
     }
 
-    public void SFXVolume(float volume)
+    public float SFXVolume(float volume)
     {
         // Ensure the input value is within the 0-100 range
         float volumePercentage = Mathf.Clamp(volume, 0.0f, 100.0f);
 
         // Convert the volumePercentage to the 0-1 range
-        volume = volumePercentage / 100.0f;
+        volume = volumePercentage/100;
         sfxSource.volume = volume;
-
-        // Save the SFX volume to PlayerPrefs
-        PlayerPrefs.SetFloat("VolumeSFX", volume);
-        PlayerPrefs.Save(); // Explicitly save PlayerPrefs data
+        return sfxSource.volume;
     }
 }
